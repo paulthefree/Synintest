0ac19f3ba3868ce8683bac1188d3e9919c3cc10d
paulthefree <52542245+paulthefree@users.noreply.github.com>
2025-05-20 21:57:12 +0100

    Add files via upload

---
+++ NewChannelCreateMyChannelMaxReceivingCapacitytest.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://widget.synonym.to/?embed=true');
+  await page.waitForSelector('#remote-balance', { timeout: 5000 })
+
+  await page.locator('#remote-balance').click();
+  await page.locator('#remote-balance').fill('1 003 009');
+  await page.getByRole('button', { name: 'Create my channel' }).click();
+});
